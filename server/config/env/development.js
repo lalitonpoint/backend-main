1a14deaf90db13b6b3e73da94e5a72c60c69d6cc
unknown <you@example.com>
2025-02-19 17:28:27 +0530

    mongodb url

---
+++ server/config/env/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: "mongodb+srv://sharmalalit842:n5n1um7sisjicn9d@cluster0.pp2zr.mongodb.net/WOW_TAXI", //wowlive
+    // db: "", //wowdev
+    encyptionSecretKey: 'secretKey' //local
+};
---
+++ server/config/env/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db: "mongodb+srv://sharmalalit842:n5n1um7sisjicn9d@cluster0.pp2zr.mongodb.net/WOW_TAXI", //wowlive
+    // db: "", //wowdev
+};
