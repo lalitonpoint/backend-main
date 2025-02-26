b96e6c8b5f6efded6c4f0a03c6e741347e72a4bc
unknown <you@example.com>
2025-02-26 16:53:19 +0530

    database change

--- mass_notification/config/env/production.js
+++ mass_notification/config/env/production.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    db: "mongodb+srv://wow:ani27rualh2vz9fq@cluster0.ii2uy.mongodb.net/WOW_TAXI", //wowlive
+    db: "mongodb+srv://WOW:ani27rualh2vz9fq@cluster0.ii2uy.mongodb.net/WOW_TAXI", //wowlive
     // db: "", //wowdev
 };
